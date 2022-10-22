28b0c48a3a53e17e26466a7302f0d3f8b06deec3
Harish-Kanthi <hkanthi3@gatech.edu>
2022-10-21 23:22:35 -0400

    Signed-off-by: Harish-Kanthi <hkanthi3@gatech.edu>
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import requests
+import csv, json, requests, re
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
 
 kroger_client_id = "instahealth-179c5f3fd5cf54b9a407f14847ee638f3255889021849445767"
 kroger_client_secret = "Mj6Lw7QcGm7f3ZGprJMK-NqVdiCUBzaFoxqK8Lyv"
+
+auth_str = "Authorization: Basic {{base64(" + kroger_client_id + ":" + kroger_client_secret + "}}"
